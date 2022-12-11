cd5dc831751ba9acb84bf8af8df53aa9a91c748b
hubsi99 <jakob_hubauer@gmx.at>
2022-12-11 10:42:33 +0100

    null-Rückgabe in getRooms() geaendert

--- parent-project-skeleton/client/src/main/java/at/jku/Client.java
+++ parent-project-skeleton/client/src/main/java/at/jku/Client.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Collections;
 import java.util.List;
 
 public class Client implements APIFunctions{
@@ -32,7 +33,7 @@ public class Client implements APIFunctions{
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
